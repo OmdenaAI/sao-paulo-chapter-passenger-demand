b78da5ce8c29f69dbaffb7213a2bf3aee0dd68a2
Matheus Ventura <107212644+matheusventurads@users.noreply.github.com>
2023-06-19 23:35:40 -0300

    add basic webapp basic sections

---
+++ src/tasks/task_4_webapp_development/matheus-ventura/pages/Solution.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config('Solution')
+
+st.markdown('# Solution')
+
+st.write(
+    """
+    Page for a more detailed explanation, steps and thoughts about the projects or decisions made.
+    """
+)
