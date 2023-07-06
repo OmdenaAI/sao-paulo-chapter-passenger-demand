b3f345f29c9d7d3f693ba73a0cb2d4a78296fd77
Arjun Angirishi <arjunangirishi1@gmail.com>
2023-07-06 22:40:39 +0530

    Update Team.py
    Team Members added

--- src/tasks/task_4_webapp_development/matheus-ventura/pages/Team.py
+++ src/tasks/task_4_webapp_development/matheus-ventura/pages/Team.py
@@ -11,10 +11,54 @@ st.set_page_config(
 
 st.sidebar.image('images/Omdena-Banner.png')
 
-st.markdown('# Team')
+st.header("Our Team")
+st.markdown("""
+    #### _Omdena São Paulo, Brazil Chapter: Monitoring and Predicting Subway Passenger Demand in São Paulo City_
+    """)
 
-st.write(
+st.text('')
+st.markdown(
     """
-    Omdena São Paulo Brazil Chapter
+        
+    ##### Chapter Lead : [Wallace Ferreira](https://www.linkedin.com/in/wallace-ferreira-69065910/)
+
+
+
+    """
+)
+
+
+st.markdown(
+    """
+    * [Anderson Marques de Almeida](https://www.linkedin.com/in/anderson-marques-de-almeida-33096546/)
+    * [Ansh Cs](https://www.linkedin.com/in/anshcs/)
+    * [Arjun Angirishi](https://www.linkedin.com/in/arjun-angirishi-4952a1201/)
+    * [Beatriz Lima](https://www.linkedin.com/in/beatrizmlima/)
+    * [Bruno Eizo Higaki](https://www.linkedin.com/in/bruno-eizo-higaki/)
+    * [Diogo Loureiro Afonso](https://www.linkedin.com/in/diogo-loureiro/)
+    * [Gustavo Cunha](https://www.linkedin.com/in/ds-gustavo-cunha/)
+    * [Hélio Júnior](https://www.linkedin.com/in/heliojunior7/)
+    * [Igor Dias](https://www.linkedin.com/in/igor-dias-1073a6175/)
+    * [Indrajith C](https://www.linkedin.com/in/indrajith-c/)     
+    * [Jordi Tuneu](https://www.linkedin.com/in/jordi-tuneu-34054326b/)   
+    * [Kausthab Dutta Phukan](https://www.linkedin.com/in/kausthab-dutta-phukan-014b5b17/)
+    * [Lucas Marques Passoni](https://www.linkedin.com/in/lucas-marques-passoni-9178a71aa/)
+    * [Luciano Lucena Rodrigues](https://www.linkedin.com/in/lucianolucena/)
+    * [Marcelo Zanoni Camargo](https://www.linkedin.com/in/mzcamargo/)
+    * [Michelle Van](https://www.linkedin.com/in/michellejtvan/)
+    * [Manpreet Jassi](https://www.linkedin.com/in/manpreet-jassi-291788166/)
+    * [Marcos Iarossi Caceres](https://www.linkedin.com/in/marcos-iarossi-caceres-856087107/)
+    * [Matheus Ventura](https://www.linkedin.com/in/matheus-ventura/)
+    * [Pedro Ribeiro](https://www.linkedin.com/in/pedro-ribeiro257/)
+    * [Rafael Carvalho Gonçalves](https://www.linkedin.com/in/rafael-carvalho-goncalves/)
+    * [Rafael Pacco](https://www.linkedin.com/in/rpacco/)
+    * [Rodrigo Vidal Cabral](https://www.linkedin.com/in/rodrigovidalcabral/)
+    * [Rodrigo Zambon](https://www.linkedin.com/in/rzambon/)
+    * [Rogerio Chaves](https://www.linkedin.com/in/rogerio-chaves/)
+    * [Saurabh Bhardwaj]()
+    * [Tarun Pala](https://www.linkedin.com/in/tarun-pala-8b909a223/)
+    * [Vandriele Barbosa](https://www.linkedin.com/in/vandriele-b/)
+    * [Whaner Endo](https://www.linkedin.com/in/whaner/)
+
     """
 )
